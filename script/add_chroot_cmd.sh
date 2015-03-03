8d8c87248d6f7a76082a12c31e27fe3c35b5e72e
Artyom Laar <artem.laar@ya.ru>
2015-03-04 01:19:43 +0800

    Added INSTALL script

--- script/add_chroot_cmd.sh
+++ script/add_chroot_cmd.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+
+get_elf_class()
+{
+#	readelf -h "$1" |
+#	sed -n "s/^\s*Class:\s*//p"
+	file -b $1 | cut -d\  -f1,2
+}
+
 mkdir chroot
 cd chroot/
-mkdir bin lib
+mkdir bin lib32 lib64
 cp `which $@` bin
 (
 	for i in $@
@@ -15,4 +23,11 @@ cp `which $@` bin
 ) |
 tee /dev/stdout |
 sort -u |
-xargs -I@ cp -v @ lib
+while read l
+do
+	case "`get_elf_class $l`" in
+	(ELF\ 32-bit)	cp -v $l lib32 ;;
+	(ELF\ 64-bit)	cp -v $l lib64 ;;
+	esac
+done
+
